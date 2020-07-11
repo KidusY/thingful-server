09d7d11f544854d3644b4d7b0558fb8d6cbc4c1b
kidus yilma <kidusyilma@gmail.com>
2020-07-11 12:47:32 -0700

    added users reg

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const usersRouter = express.Router();
 const jsonBodyParser = express.json();
-const { validatePassword,hasUserWithUserName } = require('./users-service');
+const { insertUser ,validatePassword, hasUserWithUserName, serializeUser, hashPassword } = require('./users-service');
 
 usersRouter.post('/', jsonBodyParser, (req, res, next) => {
-	const { password, userName,fullName,nickName } = req.body;
+	const { password, userName, fullName, nickName } = req.body;
 	for (const field of [ 'fullName', 'userName', 'password' ])
 		if (!req.body[field])
 			return res.status(400).json({
@@ -16,14 +16,26 @@ usersRouter.post('/', jsonBodyParser, (req, res, next) => {
 	if (passwordError) {
 		return res.status(400).json({ error: passwordError });
 	}
-	hasUserWithUserName(req.app.get('db'),userName)
-	.then(hasUserWithUserName =>{
-		if(hasUserWithUserName){
-			return res.status(400).json({error:`User Name already taken`})
-		}
-	})
+	hasUserWithUserName(req.app.get('db'), userName)
+		.then((hasUserWithUserName) => {
+			if (hasUserWithUserName) {
+				return res.status(400).json({ error: `User Name already taken` });
+			}
 
+			return hashPassword(password).then((hashedPassword) => {
+				const newUser = {
+					user_name:userName,			
+					password: hashedPassword,
+					full_name:fullName,
+					nickname:nickName					
+				};
 
+				return insertUser(req.app.get('db'), newUser).then((user) => {
+					res.status(201).json(serializeUser(user));
+				});
+			});
+		})
+		.catch(next);
 });
 
 module.exports = usersRouter;
